04b021df14fca28ce7ec38aa5cebf44e2a7efd25
Tom Jaejoon Lee <serialkk@naver.com>
2017-02-09 12:34:39 +0900

    add blood resource and prefab

---
+++ ENP2/Assets/Script/Blood.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Blood : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Invoke("DestroyNow", 0.5f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.LookAt(Camera.main.transform.position, -Vector3.up);
+    }
+
+    public void DestroyNow()
+    {
+        //transform.DetachChildren();
+        GameObject.Destroy(gameObject);
+    }
+}
